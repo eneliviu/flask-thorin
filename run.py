3b86ff4e68f19c96d8ba7b628acc661a374838e3
Liviu Ene <eneliviu@gmail.com>
2024-07-15 17:05:51 +0200

    Add templates folder

--- run.py
+++ run.py
@@ -1,17 +1,28 @@
 import os
-from flask import Flask
+from flask import Flask , render_template
 
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def index():
-    return "Hello, World!"
+def index(): # index view
+    return render_template("index.html")
+
+
+@app.route("/about")
+def about(): # about view
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def contact(): # about view
+    return render_template("contact.html")
+
 
 
 if __name__ == "__main__":
     app.run(
             host=os.environ.get("IP", "0.0.0.0"),
             port=int(os.environ.get("PORT", "5000")),
-            debug=False)
+            debug=True)
