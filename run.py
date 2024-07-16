b09b566e20edbfba95a0a9ee1a497ff9f225819d
Liviu Ene <eneliviu@gmail.com>
2024-07-16 11:50:13 +0200

    Add contact form with flashes

--- run.py
+++ run.py
@@ -1,9 +1,12 @@
 import os
 import json
-from flask import Flask , render_template
+from flask import Flask, render_template, request, flash
+if os.path.exists("env.py"):
+    import env #  creates the __pycache__ when imported
 
 
 app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY")
 
 
 @app.route("/")
@@ -31,8 +34,11 @@ def about_member(member_name):
                 member = obj
     return render_template("member.html", member=member)
 
-@app.route("/contact")
+
+@app.route("/contact", methods=["GET", "POST"])
 def contact():  # about view
+    if request.method == "POST":
+        flash(f"Thanks! { request.form.get("name") }, we have received your message!")
     return render_template("contact.html", page_title="Contact")
 
 
