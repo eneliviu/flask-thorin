47aa8c5a9131e137489bb7e334b26a2583dbba01
Liviu Ene <eneliviu@gmail.com>
2024-07-16 10:15:08 +0200

    Add variables in the views for naming page titles when rendering the html page

--- run.py
+++ run.py
@@ -12,17 +12,17 @@ def index(): # index view
 
 @app.route("/about")
 def about(): # about view
-    return render_template("about.html")
+    return render_template("about.html", page_title="About")
 
 
 @app.route("/contact")
 def contact(): # about view
-    return render_template("contact.html")
+    return render_template("contact.html", page_title="Contact")
 
 
 @app.route("/careers")
 def careers():
-    return render_template("careers.html")
+    return render_template("careers.html", page_title="Careers")
 
 
 if __name__ == "__main__":
